4104b852c7c5c234a64e0250fe86e39c3fd9bb5f
ekerbiceraykut-web <ekerbiceraykut@gmail.com>
2026-06-10 15:35:26 +0300

    fix: Windows siyah CMD penceresi gizlendi

--- run.bat
+++ run.bat
@@ -1,8 +1,6 @@
 @echo off
 cd /d "%~dp0"
 python -c "import PyQt6" 2>nul || (
-    echo Gerekli paketler yukleniyor...
     pip install -r requirements.txt
 )
-python main.py
-pause
+pythonw main.py
